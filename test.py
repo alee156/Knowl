e35e39263931921616aaefa395a4e3ee262c245e
ykim160 <yechan77@gmail.com>
2018-08-29 21:23:01 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import unittest
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        a = np.array([1, 2, 3])
+        self.assertEqual(a[0], 1)
+
+if __name__ == '__main__':
+    unittest.main()
